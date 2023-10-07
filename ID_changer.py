72c419492dd32b077eb2c68e08c37e66ae134650
hamidreza <hrhhemati@gmail.com>
2023-10-07 18:05:42 +0330

    resolved the relation tags refrence to relation ID's problem

--- ID_changer.py
+++ ID_changer.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-file_path = 'test_files/test/lanelet2_map.osm'
+file_path = 'test_files/lanelet2_map.osm'
 # Parse the .osm file
 tree = ET.parse(file_path)
 root = tree.getroot()
@@ -8,7 +8,7 @@ root = tree.getroot()
 id_mapping = {}
 new_id = 1
 
-# Process Node tags
+# update Node tags
 for node in root.findall('.//node'):
     old_id = node.get('id')
     print(old_id)
@@ -16,14 +16,14 @@ for node in root.findall('.//node'):
     node.set('id', str(new_id))
     new_id += 1
 
-# Process Way tags
+# update Way tags
 for way in root.findall('.//way'):
     old_id = way.get('id')
     id_mapping[old_id] = str(new_id)
     way.set('id', str(new_id))
     new_id += 1
 
-# Process Relation tags
+# update Relation tags
 for relation in root.findall('.//relation'):
     old_id = relation.get('id')
     id_mapping[old_id] = str(new_id)
@@ -47,10 +47,26 @@ for relation in root.findall('.//relation'):
             member.set('ref', new_ref)
 
 
-# Write the modified XML back to a file
+def update_relation_members(element):
+    for member in element.findall('.//member[@type="relation"]'):
+        old_ref = member.get('ref')
+        new_ref = id_mapping.get(old_ref)
+        if new_ref:
+            member.set('ref', new_ref)
+            # Find and process the referenced relation recursively
+            referenced_relation = root.find(f'.//relation[@id="{new_ref}"]')
+            if referenced_relation:
+                update_relation_members(referenced_relation)
+
+
+# Process Relation tags to update references to Relation IDs
+for relation in root.findall('.//relation'):
+    update_relation_members(relation)
+
+
 tree.write('new_file.osm', encoding='utf-8', xml_declaration=True)
 
-# Save the ID mapping to a separate file (if needed)
+# Save the ID mapping to a separate file for manually checking files
 with open('id_mapping.txt', 'w') as mapping_file:
     for old_id, new_id in id_mapping.items():
         mapping_file.write(f'{old_id} => {new_id}\n')
