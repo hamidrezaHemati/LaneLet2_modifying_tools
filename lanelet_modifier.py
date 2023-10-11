9f9c23c3c05ae27b0ad204ff514c8dfba68b6d13
hamidreza <hrhhemati@gmail.com>
2023-10-11 15:14:40 +0330

    comparing functionalities added to the lanelet_modifier

--- lanelet_modifier.py
+++ lanelet_modifier.py
@@ -1,5 +1,10 @@
 import xml.etree.ElementTree as ET
+
+# Path of the file you want to update its ID's
 file_path = 'test_files/lanelet2_map.osm'
+# Path of the files you want to display_comparison to each other
+file_path_A = 'compare_test/A/Shinjuku.osm'
+file_path_B = 'compare_test/B/kashiwa_standard.osm'
 
 # Create a mapping dictionary for old IDs to new IDs
 id_mapping = {}
@@ -76,11 +81,86 @@ def save_id_mapping():
             mapping_file.write(f'{old_id} => {new_id}\n')
 
 
+def tag_extractor(root_tag, root):
+    _list = []
+    # Iterate through the <node> elements (you can also do this for <way> and <relation> elements)
+    for node in root.findall(f'.//{root_tag}'):
+        # Iterate through the <tag> elements within each <node> element
+        for tag in node.findall('./tag'):
+            # Get the value of the 'k' attribute and append it to the list
+            tag_key = tag.get('k')
+            if root_tag != 'node' and (tag_key == 'type' or tag_key == 'subtype'):
+                tag_value = tag.get('v')
+                _list.append((tag_key, tag_value))
+            else:
+                _list.append(tag_key)
+
+    # Remove duplicates by converting the list to a set and back to a list (if needed)
+    return list(set(_list))
+
+
+def display_comparison(list_A, list_B):
+    set_A = set(list_A)
+    set_B = set(list_B)
+    # Elements in list A but not in list B
+    elements_in_A_not_in_B = set_A - set_B
+    # Elements in list B but not in list A
+    elements_in_B_not_in_A = set_B - set_A
+    # Elements common to both lists
+    common_elements = set_A.intersection(set_B)
+
+    # Convert the results back to lists
+    unique_to_A = list(elements_in_A_not_in_B)
+    unique_to_B = list(elements_in_B_not_in_A)
+    common_elements_list = list(common_elements)
+
+    # Print common elements in green
+    print("\033[92mCommon elements:", common_elements_list)
+    # Reset the color to default
+    print("\033[0m")
+    # Print elements in A but not in B in red
+    print("\033[91mElements in A but not in B:", unique_to_A)
+    # Reset the color to default
+    print("\033[0m")
+    # Print elements in B but not in A in red
+    print("\033[91mElements in B but not in A:", unique_to_B)
+    # Reset the color to default
+    print("\033[0m")
+    print("-----------------------------------------------------------------------")
+
+
+def compare():
+    # Parse the .osm file
+    treeA = ET.parse(file_path_A)
+    rootA = treeA.getroot()
+    treeB = ET.parse(file_path_B)
+    rootB = treeB.getroot()
+    # file A
+    node_tags_A = tag_extractor('node', rootA)
+    way_tags_A = tag_extractor('way', rootA)
+    relation_tags_A = tag_extractor('relation', rootA)
+    # file B
+    node_tags_B = tag_extractor('node', rootB)
+    way_tags_B = tag_extractor('way', rootB)
+    relation_tags_B = tag_extractor('relation', rootB)
+
+    print('Nodes')
+    display_comparison(node_tags_A, node_tags_B)
+    print('Ways')
+    display_comparison(way_tags_A, way_tags_B)
+    print('Relations')
+    display_comparison(relation_tags_A, relation_tags_B)
+
+
 def main():
     tree = ET.parse(file_path)
     root = tree.getroot()
-    update_ids(root, tree)
-    save_id_mapping()
+    # ID reset
+    # update_ids(root, tree)
+    # save_id_mapping()
+
+    # comparing lanelet files
+    compare()
 
 
 if __name__ == '__main__':
