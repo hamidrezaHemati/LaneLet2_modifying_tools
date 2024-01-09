bbe82751ec5bb02e07e1fcf4ecd37db1a6afcb83
unknown <hrhhemati@gmail.com>
2024-01-09 17:57:18 +0330

    using this code we can compare 2 lanelet2 files with each other to find if there is any difference between their tags and structure

--- lanelet_comparator.py
+++ lanelet_comparator.py
@@ -1,13 +1,8 @@
 import xml.etree.ElementTree as ET
 
-file_path_A = 'compare_test/A/Shinjuku.osm'
-file_path_B = 'compare_test/B/kashiwa_standard.osm'
-# Parse the .osm file
-treeA = ET.parse(file_path_A)
-rootA = treeA.getroot()
-
-treeB = ET.parse(file_path_B)
-rootB = treeB.getroot()
+# Path of the files you want to compare with each other
+file_path_A = 'compare_test/A/lanelet2_map.osm'
+file_path_B = 'compare_test/B/lanelet2_map.osm'
 
 
 def tag_extractor(root_tag, root):
@@ -28,7 +23,7 @@ def tag_extractor(root_tag, root):
     return list(set(_list))
 
 
-def compare(list_A, list_B):
+def venn_diagram(list_A, list_B):
     set_A = set(list_A)
     set_B = set(list_B)
     # Elements in list A but not in list B
@@ -42,7 +37,11 @@ def compare(list_A, list_B):
     unique_to_A = list(elements_in_A_not_in_B)
     unique_to_B = list(elements_in_B_not_in_A)
     common_elements_list = list(common_elements)
+    return unique_to_A, unique_to_B, common_elements_list
 
+
+def display_comparison(list_A, list_B):
+    unique_to_A, unique_to_B, common_elements_list = venn_diagram(list_A, list_B)
     # Print common elements in green
     print("\033[92mCommon elements:", common_elements_list)
     # Reset the color to default
@@ -58,22 +57,35 @@ def compare(list_A, list_B):
     print("-----------------------------------------------------------------------")
 
 
-# file A
-node_tags_A = tag_extractor('node', rootA)
-way_tags_A = tag_extractor('way', rootA)
-relation_tags_A = tag_extractor('relation', rootA)
-# file B
-node_tags_B = tag_extractor('node', rootB)
-way_tags_B = tag_extractor('way', rootB)
-relation_tags_B = tag_extractor('relation', rootB)
+def compare():
+    # Parse the .osm file
+    treeA = ET.parse(file_path_A)
+    rootA = treeA.getroot()
+    treeB = ET.parse(file_path_B)
+    rootB = treeB.getroot()
+    # file A
+    node_tags_A = tag_extractor('node', rootA)
+    way_tags_A = tag_extractor('way', rootA)
+    relation_tags_A = tag_extractor('relation', rootA)
+    # file B
+    node_tags_B = tag_extractor('node', rootB)
+    way_tags_B = tag_extractor('way', rootB)
+    relation_tags_B = tag_extractor('relation', rootB)
 
-print('Nodes')
-compare(node_tags_A, node_tags_B)
-print('Ways')
-compare(way_tags_A, way_tags_B)
-print('Relations')
-compare(relation_tags_A, relation_tags_B)
+    print('Nodes')
+    display_comparison(node_tags_A, node_tags_B)
+    print('Ways')
+    display_comparison(way_tags_A, way_tags_B)
+    print('Relations')
+    display_comparison(relation_tags_A, relation_tags_B)
 
+
+def main():
+    compare()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
